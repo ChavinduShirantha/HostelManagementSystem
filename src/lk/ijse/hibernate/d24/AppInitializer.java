ce807a840740379b38ffa73fb119823c334112ff
ChavinduShirantha <chavindushirantha@gmail.com>
2023-03-28 09:50:23 +0530

    Updated

--- src/lk/ijse/hibernate/d24/AppInitializer.java
+++ src/lk/ijse/hibernate/d24/AppInitializer.java
@@ -2,10 +2,13 @@ package lk.ijse.hibernate.d24;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 /**
  * @author : Chavindu
@@ -20,10 +23,20 @@ public class AppInitializer extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/lk/ijse/hibernate/d24/view/LoginForm.fxml"))));
+        URL resource = getClass().getResource("/lk/ijse/hibernate/d24/view/LoginForm.fxml");
+        Parent load = FXMLLoader.load(resource);
+        Scene scene = new Scene(load);
+        primaryStage.setTitle("D24 Hostel");
+        primaryStage.setResizable(false);
+        Image image = new Image("/lk/ijse/hibernate/d24/view/assest/images/logo.png");
+        primaryStage.getIcons().add(image);
+        primaryStage.setScene(scene);
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+        /*primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/lk/ijse/hibernate/d24/view/LoginForm.fxml"))));
         primaryStage.setResizable(false);
         primaryStage.setTitle("D24 Hostel");
         primaryStage.centerOnScreen();
-        primaryStage.show();
+        primaryStage.show();*/
     }
 }
